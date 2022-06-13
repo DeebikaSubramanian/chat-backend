acb662fdde4d265d707bb4a41f210cdf1f63b418
Deebika Subramanian <deebideebikask@gmail.com>
2022-06-13 19:31:08 +0300

    make it better

--- index.js
+++ index.js
@@ -28,8 +28,9 @@ mongoose
 
 const PORT=process.env.PORT || 5000;
 
-const server = app.listen(PORT, () =>
+const server = app.listen(PORT, () =>{
   console.log(`Server started on ${PORT}`)
+}
 );
 
     const io = socket(server, {
